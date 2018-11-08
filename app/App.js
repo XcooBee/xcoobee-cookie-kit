992009e22fb0fda4dd5f3eb0e65648ef2832d725
vladimir-maklyakov <maklyakov@sednor.net>
2018-11-08 18:34:39 +0200

    Implement user settings

--- app/App.js
+++ app/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import CryptoJS from "crypto-js";
 
 import CookieKitPopup from "./component/CookieKitPopup";
 import {
@@ -9,6 +10,7 @@ import {
   cookieTypes,
   requiredFields,
   consentStatuses,
+  expirationTime,
 } from "./utils";
 import graphQLRequest from "./utils/graphql";
 
@@ -32,28 +34,35 @@ export default class App extends Component {
       loading: true,
       pulsing: false,
       userOptions: [],
+      crowdAI: false,
     };
 
     this.timer = null;
     this.errors = false;
 
     this.checkRequiredFields();
-    this.fetchUserSettings();
+
+    const timestamp = localStorage[xcoobeeCookiesKey] ? JSON.parse(localStorage[xcoobeeCookiesKey]).timestamp : null;
+
+    if (timestamp && ((Date.now() - timestamp) < expirationTime)) {
+      this.fetchLocation(false, false, true);
+    } else {
+      this.fetchUserSettings();
+    }
   }
 
-  setAnimation(countryCode, crowdAI, userSettings) {
-    const { userOptions, isOffline } = this.state;
+  setAnimation(countryCode, crowdAI, userSettings, savedPreferences) {
+    const { userOptions } = this.state;
     const { config } = XcooBee.kit;
-    const savedPreferences = localStorage[xcoobeeCookiesKey];
 
-    if (savedPreferences && isOffline) {
+    if (savedPreferences) {
       config.cookies.forEach((cookie) => {
         const cookieType = cookieTypes.find(type => type.key === cookie.type);
 
-        cookie.checked = JSON.parse(savedPreferences)[cookieType.id];
+        cookie.checked = JSON.parse(localStorage[xcoobeeCookiesKey]).cookies[cookieType.id];
       });
 
-      return;
+      return this.startPulsing(animations.userSettings);
     }
     if (userSettings && localStorage[tokenKey]) {
       return this.startPulsing(animations.userSettings);
@@ -62,7 +71,7 @@ export default class App extends Component {
       return this.startPulsing(animations.crowdIntelligence);
     }
     if (userOptions.length && localStorage[tokenKey]) {
-      config.cookies.forEach(cookie => {
+      config.cookies.forEach((cookie) => {
         if (userOptions.includes(cookie.type)) {
           cookie.checked = true;
         }
@@ -70,8 +79,8 @@ export default class App extends Component {
 
       return this.startPulsing(animations.userPreference);
     }
-    if (!euCountries.includes(countryCode) && config.displayOnlyForEU && localStorage[tokenKey]) {
-      config.cookies.forEach(cookie => {
+    if (!euCountries.includes(countryCode) && config.displayOnlyForEU) {
+      config.cookies.forEach((cookie) => {
         if (config.checkByDefaultTypes.includes(cookie.type)) {
           cookie.checked = true;
         }
@@ -95,13 +104,17 @@ export default class App extends Component {
   fetchUserSettings() {
     const { campaignReference } = XcooBee.kit.config;
 
+    this.setState({ loading: true });
+
     if (campaignReference && localStorage[tokenKey]) {
       const query = `query UserConsentSettings {
         user { 
           cursor,
+          xcoobee_id,
           settings { 
             consent { 
-              accept_cookies 
+              accept_cookies,
+              use_crowd_ai
             }
           }
         }
@@ -109,19 +122,18 @@ export default class App extends Component {
 
       graphQLRequest(query, null, localStorage[tokenKey])
         .then((res) => {
-          const { accept_cookies } = res ? res.user.settings.consent : null;
-
-          if (accept_cookies) {
-            this.setState({ userOptions: accept_cookies });
+          this.setState({ crowdAI: res.user.settings.consent && res.user.settings.consent.use_crowd_ai });
+          if (res.user.settings.consent && res.user.settings.consent.accept_cookies) {
+            this.setState({ userOptions: res.user.settings.consent.accept_cookies });
           }
-          this.fetch100Sites(res.user.cursor);
+          this.fetch100Sites(res.user.cursor, res.user.xcoobee_id);
         });
     } else {
       this.fetchLocation();
     }
   }
 
-  fetch100Sites(userCursor) {
+  fetch100Sites(userCursor, xcoobeeId) {
     const query = `query SystemUserQueries($user_cursor: String!) {
       cookie_consents(user_cursor: $user_cursor) {
         site,
@@ -131,13 +143,15 @@ export default class App extends Component {
 
     graphQLRequest(query, { user_cursor: userCursor }, localStorage[tokenKey])
       .then((res) => {
-        const siteSettings = res ? res.cookie_consents[0].cookies : null;
+        const siteSettings = res.cookie_consents
+          .find(consent => consent.site === CryptoJS.SHA256(`${window.location.origin.toLowerCase()}${xcoobeeId}`)
+            .toString(CryptoJS.enc.Base64));
 
-        if (siteSettings && siteSettings.length) {
+        if (siteSettings && siteSettings.cookies.length) {
           const { config } = XcooBee.kit;
 
-          config.cookies.forEach(cookie => {
-            if (siteSettings.includes(cookie.type)) {
+          config.cookies.forEach((cookie) => {
+            if (siteSettings.cookies.includes(cookie.type)) {
               cookie.checked = true;
             }
           });
@@ -149,35 +163,39 @@ export default class App extends Component {
   }
 
   fetchCrowdAI() {
-    const query = `query CrowdRating($campaign_name: String!) {
-      crowd_rating(campaign_name: $campaign_name) {
-        cookie_type value
-      }
-    }`;
+    const { crowdAI } = this.state;
 
-    graphQLRequest(query, { campaign_name: window.location.host }, localStorage[tokenKey])
-      .then((res) => {
-        const crowdRating = res ? res.crowd_rating : null;
-
-        if (crowdRating) {
-          const { config } = XcooBee.kit;
-
-          config.cookies.forEach((cookie) => {
-            cookie.checked = crowdRating.find(item => item.cookie_type === cookie.type).value >= 0.8;
-          });
-          this.fetchLocation(true);
-        } else {
-          this.fetchLocation();
+    if (crowdAI) {
+      const query = `query CrowdRating($campaign_name: String!) {
+        crowd_rating(campaign_name: $campaign_name) {
+          cookie_type value
         }
-      });
+      }`;
+
+      graphQLRequest(query, { campaign_name: window.location.host }, localStorage[tokenKey])
+        .then((res) => {
+          const crowdRating = res ? res.crowd_rating : null;
+
+          if (crowdRating) {
+            const { config } = XcooBee.kit;
+
+            config.cookies.forEach((cookie) => {
+              cookie.checked = crowdRating.find(item => item.cookie_type === cookie.type).value >= 0.8;
+            });
+            this.fetchLocation(true);
+          }
+        });
+    } else {
+      this.fetchLocation();
+    }
   }
 
-  fetchLocation(crowdAI, userSettings) {
+  fetchLocation(crowdAI, userSettings, savedPreferences) {
     fetch("http://ip-api.com/json")
       .then(res => res.json())
       .then((res) => {
         this.setState({ countryCode: res.countryCode, loading: false });
-        this.setAnimation(res.countryCode, crowdAI, userSettings);
+        this.setAnimation(res.countryCode, crowdAI, userSettings, savedPreferences);
         this.startTimer();
       });
   }
@@ -188,7 +206,7 @@ export default class App extends Component {
     this.setState({ animation });
     setTimeout(() => this.setState({ pulsing: true }), 1000);
     setTimeout(() => this.setState({ pulsing: false }), 4500);
-    setTimeout(() => this.setState({ animation: animation.noAnimation }), 5000);
+    setTimeout(() => this.setState({ animation: animations.noAnimation }), 5000);
   }
 
   startTimer() {
@@ -203,7 +221,9 @@ export default class App extends Component {
   }
 
   handleOpen(animation) {
-    if (this.errors) {
+    const { loading } = this.state;
+
+    if (this.errors || animation !== animations.noAnimation || loading) {
       return;
     }
 
@@ -211,18 +231,14 @@ export default class App extends Component {
     clearTimeout(this.timer);
   }
 
-  handleClose(xcoobeeCookies) {
-    const { config } = XcooBee.kit;
-
-    if (xcoobeeCookies) {
-      config.cookies.forEach((cookie) => {
-        const cookieType = cookieTypes.find(type => type.key === cookie.type);
-
-        cookie.checked = xcoobeeCookies[cookieType.id];
-      });
-    }
+  handleClose() {
     this.setState({ isOpen: false });
     this.startTimer();
+  }
+
+  handleLogin() {
+    this.setState({ isOpen: false });
+    this.fetchUserSettings();
   }
 
   render() {
@@ -239,6 +255,7 @@ export default class App extends Component {
               <CookieKitPopup
                 campaign={XcooBee.kit.config}
                 onClose={cookies => this.handleClose(cookies)}
+                onLogin={() => this.handleLogin()}
                 isOffline={isOffline}
                 countryCode={countryCode}
               />
