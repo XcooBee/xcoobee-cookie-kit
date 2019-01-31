75cd3a68ae0c98f107f08dc790c8dbffa6b72fc1
vladimir-maklyakov <maklyakov@sednor.net>
2019-01-31 12:48:55 +0200

    Remove required handlers

--- packages/xcoobee-cookie-kit-web/src/index.js
+++ packages/xcoobee-cookie-kit-web/src/index.js
@@ -21,10 +21,6 @@ function checkRequiredFields(config) {
     }
   });
 
-  if (!config.cookieHandler && !config.targetUrl) {
-    errors.push("One of `cookieHandler` or `targetUrl` fields is required as initialization parameter.");
-  }
-
   if (errors.length > 0) {
     errors.forEach(errorMessage => console.error(errorMessage));
     return false;
