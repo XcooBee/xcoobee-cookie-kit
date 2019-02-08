9423d88273ececf92857cf4399fd3a38d99bf93a
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-02-08 11:38:29 +0200

    Update packages/xcoobee-cookie-kit-core/src/LocaleManager.js
    Co-Authored-By: maklyakov <30347044+maklyakov@users.noreply.github.com>

--- packages/xcoobee-cookie-kit-core/src/LocaleManager.js
+++ packages/xcoobee-cookie-kit-core/src/LocaleManager.js
@@ -31,7 +31,7 @@ export function fetchCountryCode() {
   return fetch("http://ip-api.com/json")
     .then(res => res.json())
     .then((res) => {
-      // console.log("CookieConsentsManager#fetchCountryCode fetched.");
+      // console.log("LocaleManager#fetchCountryCode fetched.");
       const countryCode = res ? res.countryCode : "US";
       return countryCode;
     });
